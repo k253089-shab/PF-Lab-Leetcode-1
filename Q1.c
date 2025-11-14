da638f22d0fd5bc0b158480017204d6a7f681393
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-11-14 10:34:36 +0500

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	int num[4];
+	printf(" Enter 4 numbers for the array\n");
+	scanf("%d %d %d %d" , &num[0], &num[1] , &num[2], &num[3]);
+	int sums[4], i,j, sum;
+
+	for(i = 0; i < 4; i++){
+		sum = 0;
+		for(j = 0; j <= i; j++){
+			sum += num[j];
+		}
+		sums[i] = sum;
+	}
+
+	printf("Old Array\t");
+	for(i = 0 ; i<4 ; i++){
+		printf("%d\t", num[i]);
+	}
+	printf("\nNew Array\t");
+	for(i = 0 ; i<4 ; i++){
+		printf("%d\t", sums[i]);
+	}
+	
+	return 0;
+}
